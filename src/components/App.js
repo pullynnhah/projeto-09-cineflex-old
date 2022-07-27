fbc57da4ae38af2c9f46ecf6eeba884ca6546629
Paula Rodrigues <paulinhaabro@gmail.com>
2022-07-27 13:40:01 -0300

    implement header and add to package

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
+import styled from "styled-components";
+
 export default function App() {
+  const URI = "https://mock-api.driven.com.br/api/v5/cineflex";
   return (
     <>
-      <h1>Hello</h1>
+      <Header>
+        <h1>Cineflex</h1>
+      </Header>
     </>
   );
 }
+
+const Header = styled.header`
+  width: 100vw;
+  height: 67px;
+
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: #c3cfd9;
+
+  h1 {
+    color: #e8833a;
+    font-size: 34px;
+    line-height: 40px;
+    text-transform: uppercase;
+  }
+`;
